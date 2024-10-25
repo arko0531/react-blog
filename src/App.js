ee7d9dffd4e53c0c2aa4f342cdf0fb5b03aa65e2
arko0531 <132333218+arko0531@users.noreply.github.com>
2024-10-25 22:44:59 +0900

    refactor/blog-27: 주소창 보호 loader 위치 변경

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider
+} from 'react-router-dom';
 import Sidebar from './components/sidebar/Sidebar';
 import MainPostPage from './pages/main/Index';
 import DetailPostPage from './pages/post/DetailPostPage';
@@ -11,7 +15,6 @@ import store from './store';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './util/http';
 import { app } from './firebase';
-import { checkAuthLoader } from './util/http';
 
 function App() {
   const router = createBrowserRouter([
@@ -47,3 +50,13 @@ function App() {
 }
 
 export default App;
+
+const checkAuthLoader = () => {
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    return redirect('/auth?mode=login');
+  }
+
+  return null;
+};
